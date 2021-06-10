d51af521c9f30e2625068fc015a23a6171ab1fe4
Cookiez <kukizk@gmail.com>
2021-06-10 02:11:07 +0200

    gui 2 3 4

--- Zadania/Gui/kukiz.krzysztof.lista_02/zad_01/zad_01/Program.cs
+++ Zadania/Gui/kukiz.krzysztof.lista_02/zad_01/zad_01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace zad_01
 {
@@ -7,35 +8,67 @@ namespace zad_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Lista lista = new Lista(10);
-            Lista1 lista1 = new Lista1();
-            Console.WriteLine(lista);
-            Console.WriteLine(lista1);
+            Console.WriteLine("Lista 1");
+            //{} < {1} < {1,4,1,5,8,5,9} < {2} < {2,1} < {2,4,10,3} < {2,5,8} <{2,6}
+            List<Lista1> listy1 = new List<Lista1>();
+            listy1.Add(new Lista1(new List<int> { }));
+            listy1.Add(new Lista1(new List<int> { 1 }));
+            listy1.Add(new Lista1(new List<int> { 2 }));
+            listy1.Add(new Lista1(new List<int> { 2, 1 }));
+            listy1.Add(new Lista1(new List<int> { 2, 6 }));
+            listy1.Add(new Lista1(new List<int> { 2, 5, 8 }));
+            listy1.Add(new Lista1(new List<int> { 2, 4, 10, 3 }));
+            listy1.Add(new Lista1(new List<int> { 1, 4, 1, 5, 8, 5, 9 }));
+            for (int i = 0; i < listy1.Count; i++) Console.WriteLine(listy1[i]);
+            listy1.Sort();
+            Console.WriteLine("\\/ \\/ \\/");
+            for (int i = 0; i < listy1.Count; i++) Console.WriteLine(listy1[i]);
+            Console.WriteLine();
+
+            Console.WriteLine("Lista 2");
+            //{} < {1} < {2} < {2,1} < {2,6} < {2,5,8} < {2,4,10,3} < {1,4,1,5,8,5,9}
+            List<Lista2> listy2 = new List<Lista2>();
+            listy2.Add(new Lista2(new List<int> { }));
+            listy2.Add(new Lista2(new List<int> { 1 }));
+            listy2.Add(new Lista2(new List<int> { 1, 4, 1, 5, 8, 5, 9 }));
+            listy2.Add(new Lista2(new List<int> { 2 }));
+            listy2.Add(new Lista2(new List<int> { 2, 1 }));
+            listy2.Add(new Lista2(new List<int> { 2, 4, 10, 3 }));
+            listy2.Add(new Lista2(new List<int> { 2, 5, 8 }));
+            listy2.Add(new Lista2(new List<int> { 2, 6 }));
+            for (int i = 0; i < listy2.Count; i++) Console.WriteLine(listy2[i]);
+            listy2.Sort();
+            Console.WriteLine("\\/ \\/ \\/");
+            for (int i = 0; i < listy2.Count; i++) Console.WriteLine(listy2[i]);
+            Console.WriteLine();
         }
     }
 
     class Lista
     {
         protected List<int> lista = new List<int>();
-        public Lista(int n)
-        {
-            Random rnd = new Random();
-            this.lista.Clear();
-            for (uint i = 0; i < n; i++)
-            {
-                this.lista.Add(rnd.Next() % 100 + 1);
-            }
-        }
+        Random rnd = new Random();
         public Lista()
         {
-            Random rnd = new Random();
             int listSize = rnd.Next() % 5 + 1;
             this.lista.Clear();
             for (uint i = 0; i < listSize; i++)
             {
                 this.lista.Add(rnd.Next() % 100 + 1);
             }
+        }
+        public Lista(int n)
+        {
+            this.lista.Clear();
+            for (uint i = 0; i < n; i++)
+            {
+                this.lista.Add(rnd.Next() % 100 + 1);
+            }
+        }
+        public Lista(List<int> l)
+        {
+            this.lista.Clear();
+            this.lista = l;
         }
         public override string ToString()
         {
@@ -55,8 +88,69 @@ namespace zad_01
         }
     }
 
-    class Lista1 : Lista
+    class Lista1 : Lista, IComparable<Lista1>
     {
-        Lista lista = new Lista();
+        public Lista1(List<int> l) : base(l) { }
+        public int CompareTo(Lista1 other)
+        {
+            int n = this.lista.Count > other.lista.Count ? this.lista.Count : other.lista.Count;
+            for (int i = 0; i < n; i++)
+            {
+                if (this.lista.Count < i + 1)
+                {
+                    return -1;
+                }
+                else if (other.lista.Count < i + 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    if (this.lista[i] > other.lista[i])
+                    {
+                        return 1;
+                    }
+                    else if (this.lista[i] < other.lista[i])
+                    {
+                        return -1;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+
+
+    class Lista2 : Lista, IComparable<Lista2>
+    {
+        public Lista2(List<int> l) : base(l) { }
+        public int CompareTo([AllowNull] Lista2 other)
+        {
+            if (this.lista.Count > other.lista.Count)
+            {
+                return 1;
+            }
+            else if (this.lista.Count < other.lista.Count)
+            {
+                return -1;
+            }
+            else
+            {
+                if (this.lista == other.lista)
+                {
+                    return 0;
+                }
+                else
+                {
+                    for (int i = 0; i < this.lista.Count; i++)
+                    {
+                        if (this.lista[i] > other.lista[i]) return 1;
+                        if (this.lista[i] < other.lista[i]) return -1;
+                    }
+                }
+            }
+            return 0;
+        }
+
     }
 }
---
+++ Zadania/Gui/kukiz.krzysztof.lista_03/lista_03_zad_01/lista_03_zad_01/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace lista_03_zad_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tworca Jahwe = new Tworca();
+
+            // krok 0
+            Jahwe.CreateNew("Ania");
+            Jahwe.WypiszObserwatorow(0);
+
+            // krok 1
+            Jahwe.CreateNew("Bartek");
+            Jahwe.WypiszObserwatorow(1);
+
+            // krok 2
+            Jahwe.CreateNew("Celvin");
+            Jahwe.WypiszObserwatorow(2);
+
+            // krok 3
+            Jahwe.CreateNew("Dawid");
+            Jahwe.WypiszObserwatorow(3);
+
+            // krok 4
+            Jahwe.CreateNew("Emilia");
+            Jahwe.WypiszObserwatorow(4);
+
+            // krok 5
+            Jahwe.CreateNew("Falafel");
+            Jahwe.WypiszObserwatorow(5);
+        }
+    }
+
+    class Tworca
+    {
+        List<Obserwator> Obserwatorzy = new List<Obserwator>();
+        Obserwator ObserwatorzyTmp;
+
+        public void CreateNew(string name = "NoName")
+        {
+            ObserwatorzyTmp = new Obserwator(name);
+            Nowy(ObserwatorzyTmp);
+            Obserwatorzy.Add(ObserwatorzyTmp);
+        }
+
+        void Nowy(Obserwator ObserwatorzyTmp)
+        {
+            Sasiedzi tmpSasiad;
+            for (int i = 0; i < Obserwatorzy.Count; i++)
+            {
+                //Obserwatorzy[i];
+                tmpSasiad = new Sasiedzi(Obserwatorzy[i].X, Obserwatorzy[i].Y, ObserwatorzyTmp.X, ObserwatorzyTmp.Y, ObserwatorzyTmp.Name);
+                Obserwatorzy[i].AddSasiad(tmpSasiad);
+            }
+        }
+
+        public void WypiszObserwatorow(string krok = "0")
+        {
+            Console.WriteLine($"--------------------krok {krok}-------------------");
+            for (int i = 0; i < Obserwatorzy.Count; i++)
+            {
+                Obserwatorzy[i].Wypisz();
+            }
+            Console.WriteLine();
+        }
+
+        public void WypiszObserwatorow(int krok = 0)
+        {
+            this.WypiszObserwatorow(krok.ToString());
+        }
+    }
+
+    class Obserwator
+    {
+        double x, y;
+        string name;
+        Random rnd = new Random();
+        List<Sasiedzi> moiSasiedzi = new List<Sasiedzi>();
+
+        public Obserwator(string name)
+        {
+            this.x = (double)rnd.Next() / Int32.MaxValue;
+            this.y = (double)rnd.Next() / Int32.MaxValue;
+            // zaokraglij
+            this.x = Math.Round(this.x, 3);
+            this.y = Math.Round(this.y, 3);
+            this.name = name;
+        }
+
+        public void AddSasiad(Sasiedzi newSasiad)
+        {
+            moiSasiedzi.Add(newSasiad);
+            this.moiSasiedzi.Sort();
+        }
+
+        public void Wypisz()
+        {
+            Console.WriteLine($"Jestem {this.name} - Lista sąsiadów:");
+            for (int i = 0; i < this.moiSasiedzi.Count; i++)
+            {
+                Console.WriteLine(this.moiSasiedzi[i]);
+                if (i >= 1) break;
+            }
+        }
+
+        public double X
+        {
+            get
+            {
+                return this.x;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+    }
+
+    class Sasiedzi : IComparable<Sasiedzi>
+    {
+        double x, y, odl;
+        string name;
+
+        public Sasiedzi(double myX, double myY, double sonsiadX, double sonsiadY, string name)
+        {
+            this.x = sonsiadX;
+            this.y = sonsiadY;
+            this.name = name;
+            this.odl = Math.Sqrt(Math.Pow(sonsiadX - myX, 2) + Math.Pow(sonsiadY - sonsiadX, 2));
+            this.odl = Math.Round(this.odl, 3);
+        }
+
+        public int CompareTo([AllowNull] Sasiedzi other)
+        {
+            return this.odl > other.odl ? 1 : 0;
+        }
+
+        public override string ToString()
+        {
+            return $"\t{this.name}\t\tx={this.x}\t\ty={this.y}\t\todl={this.odl}";
+        }
+    }
+}
